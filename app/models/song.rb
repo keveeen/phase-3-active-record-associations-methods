42e34f5b228747a9221b2213cf0b11bde81d62fc
keveeen <105822487+keveeen@users.noreply.github.com>
2022-11-15 22:33:08 +0100

    commit tree ac816aaea3dbc369dad36bc7abf2f5dde470dda3

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,6 +3,8 @@ class Song < ActiveRecord::Base
   belongs_to :genre
 
   def get_genre_name
+
+    self.genre.name
   end
 
   def drake_made_this
@@ -10,5 +12,16 @@ class Song < ActiveRecord::Base
     # Drake doesn't exist in the database as an artist yet, so you'll have to create a record
     # Hint: you won't want to create an artist record every time this method is called, only if Drake is *not found*
 
+    drake = Artist.find_or_create_by(name: "Drake")
+
+      self.update(artist: drake)
+
   end
 end
+
+# # Find the first user named "Scarlett" or create a new one with a
+# # different last name.
+# User.find_or_create_by(first_name: 'Scarlett') do |user|
+#   user.last_name = 'Johansson'
+# end
+# # => #<User id: 2, first_name: "Scarlett", last_name: "Johansson">
